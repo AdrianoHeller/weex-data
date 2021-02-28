5107a979466f0a4e2dd99ffdea39f2580b71eb75
Adriano Heller Mylla <adriano@technoizz.com>
2021-02-28 15:15:02 -0300

    adjusting dockerfile and docker-compose

--- src/db.ts
+++ src/db.ts
@@ -5,10 +5,10 @@ import { join } from 'path';
 config({ path: join(__dirname,'../.env')});
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_DEV_ENV,
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DBNAME,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_CMP_DB,      
+    password: process.env.MYSQL_USER_PWD,
 });
 
 const Main = async():Promise<any> => {
@@ -21,6 +21,7 @@ const Main = async():Promise<any> => {
 };
 
 Main()
+.then(db => console.log(db))
 .catch(err => console.error(err));
 
 export default connection;
