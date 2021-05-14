1047a43a5b106e51f70a19868739f8263f156a4f
Adriano Heller Mylla <adriano@technoizz.com>
2021-05-13 23:10:36 -0300

    chore: Fix Env Configs

--- src/keys.ts
+++ src/keys.ts
@@ -3,14 +3,21 @@ import { join } from 'path';
 
 config({path: join(__dirname,'../.env')});
 
-
 interface IEnvProps{
     SERVER_URL: string,
     PORT: number,
     HASH_SCRT: string,
     MONGO_USER: string,
     MONGO_PWD: string,
-    MONGO_DB_NAME: string
+    MONGO_DB_NAME?: string
+};
+
+const environmentValues:IEnvProps = {
+    HASH_SCRT: process.env.HASH_SCRT!,
+    PORT: Number(process.env.PORT),
+    SERVER_URL: process.env.SERVER_URL!,
+    MONGO_USER: process.env.MONGO_USER!,
+    MONGO_PWD: process.env.MONGO_PWD!
 };
 
 export const checkActualRunningEnvironment = () => {
