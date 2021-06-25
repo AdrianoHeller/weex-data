e1bf56085f453ba59c60434ddb024ae8eadba8e7
Adriano Heller Mylla <adriano@technoizz.com>
2021-06-24 23:45:31 -0300

    feat: Unify Servers

---
+++ src/filesNameFilter.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
+const filesNameFilter = (fileName: string) => {
+    const filesList = fs.readdirSync(path.join(__dirname,'../../uploads'));
+    const filteredImage = filesList.filter((names) => {
+        if (names.includes(fileName)){
+            return names
+        }
+    })
+    return filteredImage
+}
+
+export default filesNameFilter
